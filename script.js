5b8260a0e4ddd16c04cbd7ed4e49b95d0437a6dd
adityaprakassahoo-blip <adityaprakassahoo@gmail.com>
2026-07-13 18:40:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 function showGallery() {
+
     document.getElementById("gallery").style.display = "block";
 
-    const audio = document.getElementById("birthdayAudio");
-    audio.play();
+    const images = [
+        "./images/WhatsApp Image 2026-07-13 at 4.29.36 PM.jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.29.37 PM.jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.29.38 PM.jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.29.38 PM (1).jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.29.38 PM (2).jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.29.39 PM.jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.29.39 PM (1).jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.29.39 PM (2).jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.29.40 PM.jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.29.40 PM (1).jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.44.12 PM.jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.44.13 PM.jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.44.13 PM (1).jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.44.14 PM.jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.44.15 PM.jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.44.16 PM.jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.44.17 PM.jpeg",
+        "./images/WhatsApp Image 2026-07-13 at 4.44.17 PM (1).jpeg"
+    ];
+
+    let current = 0;
+
+    const slide = document.getElementById("slideshow");
+
+    slide.src = images[0];
+
+    setInterval(function () {
+        current++;
+        if (current >= images.length) {
+            current = 0;
+        }
+        slide.src = images[current];
+    }, 3000);
 }
