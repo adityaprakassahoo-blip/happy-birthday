ef2c6969a516b6c0f4d20f6d90caedf5a37ee31e
adityaprakassahoo-blip <adityaprakassahoo@gmail.com>
2026-07-13 17:57:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showGallery() {
 
-    var audio = new Audio("WhatsApp Audio 2026-07-13 at 5.45.47 PM.mp4");
+    var audio = new Audio("birthday.mp4");
     audio.play();
 
     document.getElementById("gallery").style.display = "block";
