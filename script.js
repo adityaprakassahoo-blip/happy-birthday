d2a618296d0a8dcf4c1fc0761dde37085aac83fd
adityaprakassahoo-blip <adityaprakassahoo@gmail.com>
2026-07-13 18:15:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function showGallery() {
-    var audio = new Audio("birthday.mp3");
-    audio.play();
+    // Play birthday voice
+    const audio = new Audio("birthday.mp3");
+    audio.play().catch(function(err){
+        console.log(err);
+    });
 
+    // Show gallery
     document.getElementById("gallery").style.display = "block";
 }
